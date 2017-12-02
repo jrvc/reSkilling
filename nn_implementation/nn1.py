54b2a1aeaa9f5e654fbc3562de47d8387c28c93c
J Raul Vazquez <jraulvc@gmail.com>
2017-12-02 15:36:31 +0200

    implementation of the optimization algorithm

--- nn_implementation/nn1.py
+++ nn_implementation/nn1.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 30 11:13:56 2017
+Created on Nov 2017
 
-@author: Raul Vazquez
-"""
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import datasets
-import scipy.optimize as op
-
-# Generate a dataset and plot it
-np.random.seed(0)
-X, y = datasets.make_moons(200, noise=0.20)
-plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
-
-
-'''
 Build a 3-layer neural network with 1 input layer, 1 hidden layer and 1 output layer. 
     The number of nodes in the input  layer is 2; the dimensionality of our data.
     The number of nodes in the output layer is 2; the number of classes we have.
@@ -52,100 +38,67 @@ MODELS:
         # OUTPUT layer activation values
         z3 = a2.dot(W2) + b2
         a3 = utils_loc.sigmoid(z3) 
-        
-'''
 
-num_examples = len(X) # training set size
-m = len(X)
+@author: Raul Vazquez
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+import scipy.optimize as op
+import utils_loc
+
+# Generate a dataset and plot it
+np.random.seed(100)
+X, y = datasets.make_moons(200, noise=0.25)
+plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
+
+
+m = len(X) # training set size
 nn_input_dim = X.shape[1] # input layer dimensionality
 nn_output_dim = 2 # output layer dimensionality
-nn_hdim = 4 # hidden layer dmensionality
+''' IMPORTANT PARAMETER: play with nn_hdim to see how the decision boundary changes'''
+nn_hdim = 4 # hidden layer dmensionality (3 seems to be the optimal, 4 already overfitts the data)
  
 # Gradient descent parameters
 epsilon = 0.01 # learning rate for gradient descent
 reg_lambda = 0.01 # regularization strength
 
 # Initialize the parameters to random values. We need to learn these.
-np.random.seed(0)
+np.random.seed(10)
 W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim)
+cardW1 = nn_input_dim * nn_hdim
 b1 = np.zeros((1, nn_hdim))
 W2 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim)
+#cardW2 = nn_hdim * nn_output_dim
 b2 = np.zeros((1, nn_output_dim))
 
-def nn_costFn_TanH(X, y, W1, b1, W2, b2, reg_lambda):
-    '''
-    Forward propagation to compute the value of the loss function
-    Backward propagation to compute the value of the gradient of the loss function 
-    '''
-    
-    """TO DO:
-        Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
-% for our 2 layer neural network
-Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
-                 hidden_layer_size, (input_layer_size + 1));
+# generate an unrolled vector of parameters
+initial_grad = np.append(np.append(b1, (W1).ravel()), np.append(b2, (W2).ravel() ) )
 
-Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
-                 num_labels, (hidden_layer_size + 1));
-    """
-    m = len(X)
-    #FWD
-    a1 = X.copy()
-    z2 = a1.dot(W1) + b1
-    a2 = np.tanh(z2)
-    z3 = a2.dot(W2) + b2
-    exp_scores = np.exp(z3)
-    a3 = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-    ''' Recall that each row of a3 contains the probabilities of belonging to each class '''    
-    # Calculating the loss
-    ''' this is the same than computing the:
-        Loss = -(1/m) * sum_1^m(sum_1^K (y[i,k] * log(a3[i,k]) + (1 - y[i,k]) * log(1 - a3[i,k])  ))
-        for Y being a matrix representation of y with boolean categorical rows 
-    '''
-    corect_logprobs = -np.log(a3[range(m), y]) # choose the probability of having the observed output for each example
-    data_loss = 1/m * np.sum(corect_logprobs)
-    # Add regulatization term to the loss
-    data_loss += reg_lambda/(2*m) * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
-    
-    # BackWD
-    delta3 = a3.copy()
-    delta3[range(m), y] -= 1 # recall delta3 = a3 - y, when y defd as a matrix with boolean canonical entries
-    delta2 = delta3.dot(W2.T) * (1 - np.power(a2, 2))
-    # derivatives
-    dW2 = (a2.T).dot(delta3)
-    db2 = np.sum(delta3, axis=0, keepdims=True)
-    dW1 = np.dot(a1.T, delta2)
-    db1 = np.sum(delta2, axis=0)
- 
-    # Add regularization terms (b1 and b2 are bias terms: don't have regularization terms)
-    dW2 += reg_lambda * W2
-    dW1 += reg_lambda * W1
- 
-    # unroll the gradients
-    grad = np.append(np.append(db1, (dW1.T).ravel()), np.append(db2, (dW2.T).ravel() ) )
-    return (data_loss, grad)
-
-
-
-
+# minimize the desired function
 '''
 if 'jac' is a boolean and is True, 'fun' is assumed to return the gradient alog with the objective function.:
     If False, the gradient will be estimated numerically
 '''
-initial_grad = np.append(np.append(b1, (W1.T).ravel()), np.append(b2, (W2.T).ravel() ) )
-Result = op.minimize(fun = nn_costFn_TanH, # can choose nn_costFn_TanH OR nn_costFn_SIGMOID
-                                 x0 = initial_grad), 
-                                 args = (X, y),
-                                 method = 'TNC',
-                                 jac = True);
+def caller(x):
+    return utils_loc.nn_costFn_TanH(x, nn_input_dim, nn_hdim, nn_output_dim, X, y, reg_lambda)
+
+result = op.minimize(fun = caller, x0 = initial_grad, jac = True)
+# for fun one can choose nn_costFn_TanH OR nn_costFn_SIGMOID    
+result = op.minimize(fun = utils_loc.nn_costFn_TanH, x0 = initial_grad,
+                     args = (nn_input_dim, nn_hdim, nn_output_dim, X, y,  reg_lambda), 
+                     jac = True)
+
+new_b1 = result.x[0:nn_hdim]
+new_W1 = np.reshape( result.x[nn_hdim:(nn_hdim + cardW1)], (nn_input_dim, nn_hdim) )
+new_b2 = result.x[(nn_hdim + cardW1):(nn_hdim + cardW1 + nn_output_dim)]
+new_W2 = np.reshape( result.x[(nn_hdim + cardW1 + nn_output_dim):], (nn_hdim, nn_output_dim) )
+    
+
+utils_loc.plot_decision_boundary( (lambda x: utils_loc.predict(W1, b1, W2, b2, x)), X, y )
+plt.title("Decision Boundary of initial Parameters")
                      
-def predict(W1, b1, W2, b2, x):
-    # Forward propagation
-    a1 = x.copy()
-    z2 = a1.dot(W1) + b1
-    a2 = np.tanh(z2)
-    z3 = a2.dot(W2) + b2
-    exp_scores = np.exp(z3)
-    a3 = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-    return np.argmax(a3, axis=1)
+utils_loc.plot_decision_boundary( (lambda x: utils_loc.predict(new_W1, new_b1, new_W2, new_b2, x) ), X,y)
+plt.title("Decision Boundary for hidden layer size "+ str( nn_hdim))
 
 
